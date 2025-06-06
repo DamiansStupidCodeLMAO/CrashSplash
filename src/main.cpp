2943ecdac1b1b0cdfa57f0ef9556b3863d74f817
Damienne <143151205+DamiansStupidCodeLMAO@users.noreply.github.com>
2025-06-06 12:28:55 -0230

    Remove the reboot bits, BSoD joke
    i chose to remove rebooting from this because, quite frankly, it was half broken in the original haltfix (no hate to shutterbug, this is a really cool plugin idea but a difficult one to pull off) and it probably leaves the system in a somewhat unstable state anyway

--- src/main.cpp
+++ src/main.cpp
@@ -15,50 +15,40 @@
 #include <utils/logger.h>
 #include <coreinit/screen.h>
 #include <mocha/mocha.h>
-#include <coreinit/ios.h>
 
 /**
     Mandatory plugin information.
     If not set correctly, the loader will refuse to use the plugin.
 **/
-WUPS_PLUGIN_NAME("HaltFix");
-WUPS_PLUGIN_DESCRIPTION("Reboots the Wii U upon a game crash");
+WUPS_PLUGIN_NAME("CrashSplash");
+WUPS_PLUGIN_DESCRIPTION("Displays a silly splash screen when the Wii U crashes");
 WUPS_PLUGIN_VERSION("v1.0");
-WUPS_PLUGIN_AUTHOR("shutterbug2000");
+WUPS_PLUGIN_AUTHOR("Damienne");
 WUPS_PLUGIN_LICENSE("ISC");
 
 extern "C" {
     void OSSendAppSwitchRequest(uint32_t rampid, void* args, uint32_t argsSize);
 }
 
-#define IOCTL_SVC            0x02
-
-#define ALIGN(align)                 __attribute__((aligned(align)))
-#define ALIGN_0x40                   ALIGN(0x40)
-
-int haxHandle;
-
 DECL_FUNCTION(void, PPCHalt) {
-    //apparently the below line isn't needed after all? it seemed to be at one point, gonna leave it here in case lol
-    //OSSendAppSwitchRequest(0,0,0); //from what I can tell, staying in the crashed process causes the reboot to fail. So I switch to something else (HOME Menu overlay)
     OSScreenInit();
     OSScreenEnableEx(SCREEN_TV, 1);
     OSScreenEnableEx(SCREEN_DRC, 1);
     uint32_t tvSize = OSScreenGetBufferSizeEx(SCREEN_TV);
     OSScreenSetBufferEx(SCREEN_TV, (unsigned char*)0xf4000000);
     OSScreenSetBufferEx(SCREEN_DRC, (unsigned char*)0xf4000000+tvSize);
-    OSScreenClearBufferEx(SCREEN_TV,0x0000ffff);
-    OSScreenClearBufferEx(SCREEN_DRC,0x0000ffff);
-    OSScreenPutFontEx(SCREEN_TV,0,0,"An error has occurred in the running game or application.\nYour Wii U console will now reboot.");
-    OSScreenPutFontEx(SCREEN_DRC,0,0,"An error has occurred in the running game or application.\nYour Wii U console will now reboot.");
+    OSScreenClearBufferEx(SCREEN_TV,0x007ad2ff);
+    OSScreenClearBufferEx(SCREEN_DRC,0x007ad2ff);
+    OSScreenPutFontEx(SCREEN_TV,0,0,":(");
+    OSScreenPutFontEx(SCREEN_TV,0,1,"Your Nintendo Wii U ran into a problem, and needs to restart.");
+    OSScreenPutFontEx(SCREEN_TV,0,2,"Please hold down the power button.");
+    OSScreenPutFontEx(SCREEN_DRC,0,0,":(");
+    OSScreenPutFontEx(SCREEN_DRC,0,1,"Your Nintendo Wii U ran into a problem, and needs to restart.");
+    OSScreenPutFontEx(SCREEN_DRC,0,2,"Please hold down the power button.");
     OSScreenFlipBuffersEx(SCREEN_TV);
     OSScreenFlipBuffersEx(SCREEN_DRC);
     OSSleepTicks(OSMillisecondsToTicks(5000));
-    ALIGN_0x40 uint32_t arguments[0x40 >> 2];
-    arguments[0] = 0x74;
-
-    ALIGN_0x40 int result[0x40 >> 2];
-    int res = IOS_Ioctl(haxHandle, IOCTL_SVC, arguments, (1 + 0) * 4, result, 4);
+    
     return;
 }
 
@@ -72,10 +62,6 @@ ON_APPLICATION_START() {
     WHBLogUdpInit();
     WHBLogCafeInit();
     DEBUG_FUNCTION_LINE("HaltFix loaded");
-
-    //this ensures the reboot works by giving this plugin a direct iosuhax handle. idk why, but the one from libmocha sometimes seems to... vanish? by the time the reboot is attempted??
-    haxHandle = IOS_Open((char *) ("/dev/iosuhax"), static_cast<IOSOpenMode>(0));
-    DEBUG_FUNCTION_LINE("haxHandle %x!", haxHandle);
 
     auto res = Mocha_InitLibrary();
 
